a4c7a7719fc9c43b0e7b2894c16868a841c3d6b5
Andrei Bulai <andrei.bulai@solera.com>
2022-11-18 17:10:34 +0100

    Our Solution for Bug Hunting :)

--- BugHunting/src/TestingBugHunting/BugHuntingSolutions.java
+++ BugHunting/src/TestingBugHunting/BugHuntingSolutions.java
@@ -4,10 +4,16 @@ import java.util.Arrays;
 
 public class BugHunting {
 
+	/*
+	 * PROBLEM 1
+	 * If condition will always return false
+	 * Solution:
+	 	 change "other.bounds[j]" to "other.bounds[j+1]"
+	 */
 	private boolean checkDimensions(CLIQUEUnit other, int e) {
 		
 		for (int i = 0, j = 0; i < e; i++, j+=2) {
-			if(dims[i] != other.bounds[j]
+			if(dims[i] != other.bounds[j+1]
 				|| bounds[j] != other.bounds[j]
 				|| bounds[j+1] != bounds[j+1]) {
 				return false;
@@ -17,13 +23,20 @@ public class BugHunting {
 	}
 	
 	
-	
+	/*
+	 * PROBLEM 2
+	 * This method returns the single element of an arraylist with size of 1.
+	 * Solution: If the size of the arraylist is 1 then returns the element with index 0. (!!! not 1)
+	 */
 	public double[] computeMean() {
-		return points.size() == 1 ? points.get(1) : null;
+		return points.size() == 1 ? points.get(0) : null;
 	}
 	
-	
-	
+	/*
+	 * PROBLEM 3
+	 * In the next method the referenceSetSize must be greater than 0.
+	 * Solution: in the first if: if(refereceSetSize <= 0)...
+	 */
 	protected PreDeConModel computeLocalModel(DoubleDBIDList neighbors, ...) {
 		final int referenceSetSize = neighbors.size();
 		//...
@@ -39,28 +52,41 @@ public class BugHunting {
 	}
 	
 	
-	
+	/*
+	 * PROBLEM 4
+	 * We want to get only stripped strings from the user.
+	 * Solution: 
+	 * 1. if the character is not a blank space, a tab, a special character etc.
+	 * 				then save add the char to the string result.
+	 * 2. return the string result.
+	 */
 	public String getStrippedSubstring(int start, int end) {
         int sstart = start, ssend = end;
         String result="";
         while(sstart < ssend) {
             char c = input.charAt(sstart);
-            if(c != ' ' || c!= '\n' || c!= '\r' || c!='\t') {
-            	break;
-            }
-  
+            if(c != ' ' || c!= '\n' || c!= '\r' || c!='\t')
+                result +=c;
+             else break;
+            
+      
             ++sstart;
         }
         return result;
         //...
     }
 	
-	
-
+	/*
+	 * PROBLEM 5
+	 * if (endKey[i] < 0xff) // this If will always return true.
+	 *  because byte type is NOT unsigned [-128, 127]. So instead of 255 (in hexadecimal) we put 127 (hexadecimal)
+	 *  Solution:
+	 *  In if: "endKey[i] < 0xff" -> "endKey[i] < 0x7F"
+	 */
 	public static final ByteSequence prefixEndOf(ByteSequence prefix) {
 		byte[] endKey = prefix.getBytes().clone();
 		for(int i = endKey.length - 1; i>=0; i--) {
-			if (endKey[i] < 0xff) {
+			if (endKey[i] < 0x7F) {
 				endKey[i] = (byte) (endKey[i]+1);
 				return ByteSequence.from(Arrays.copyOf(endKey, i+1));
 			}
@@ -68,20 +94,30 @@ public class BugHunting {
 		return ByteSequence.from(NO_PREFIX_END);
 	}
 	
-
-	
+	/*
+	 * PROBLEM 6
+	 * 1. "(int)x < 0" is always false (unsigned in Java)
+	 * 2. when (int)x == index_64.length will throw indexing beyond the bounds of the index_64
+	 * Solution:  "(int)x > index_64.length" change to "(int)x >= index_64.length"
+	 */
 	private static byte char64(char x) {
-		if((int)x < 0 || (int)x > index_64.length)
+		if((int)x < 0 || (int)x >= index_64.length)
 			return -1;
-		return index_64[(int)x];
+		return index_64[(int)x]l
 	}
 	
 	
+	/*
+	 * PROBLEM 7
+	 * In the first If statement dataTmpFile is null (must be different)
+	 * And instead of "||" it needs to be "&&"
+	 * Solution: In If statement change "==" to "!="
+	 */
 	
 	public void putToCache(PutRecordsRequest putRecordsRequest)
 	{
 	  //...
-	  if (dataTmpFile == null || !dataTmpFile.exists())
+	  if (dataTmpFile != null && !dataTmpFile.exists())
 	  {
 	    try
 	    {
@@ -96,8 +132,11 @@ public class BugHunting {
 	  //...
 	}
 	
-
-	
+	/*
+	 * PROBLEM 8
+	 * If the value of "bitShiftsInWord" is 0 the final line of code will return "1 << -1" which is a negative number.
+	 * Solution: assert that bitShiftsInWord >= 1
+	 */
 	private void shiftRightDestructive(int wordShifts,
             int bitShiftsInWord,
             boolean roundUp) 
@@ -107,7 +146,7 @@ public class BugHunting {
 		}
 	
 		assert (wordShifts >= 0);
-		assert (bitShiftsInWord >= 0);
+		assert (bitShiftsInWord >= 1);
 		assert (bitShiftsInWord < 32);
 		if (wordShifts >= 4) {
 			zeroClear();
@@ -123,8 +162,12 @@ public class BugHunting {
 		//...
 	}
 	
-
-	
+	/*
+	 * PROBLEM 9
+	 * Beacause val is a long type
+	 * We have to change the last line of code from "val = val | (1 << j);" to "val = val | ((long)1 << j);"
+	 *  Casting the value 1 to long type.
+	 */
 	public void logSargResult(int stripeIx, boolean[] rgsToRead)
 	{
 	  //...
@@ -134,7 +177,7 @@ public class BugHunting {
 	      int ix = valOffset + j;
 	      if (rgsToRead.length == ix) break;
 	      if (!rgsToRead[ix]) continue;
-	      val = val | (1 << j);
+	      val = val | ((long)1 << j);
 	    }
 	    //...
 	  }
@@ -142,7 +185,11 @@ public class BugHunting {
 	}
 	
 	
-	
+	/*
+	 * PROBLEM 10
+	 * One of the parameter was not assign to the actual class argument (whit ".this")
+	 * Solution: change "processedData == processedData" to "processedData == that.processedData"
+	 */
     public boolean equals (Object o) {
         //...
         CheckPointStatistics that = (CheckPointStatistics) o;
@@ -153,7 +200,7 @@ public class BugHunting {
                 statesize == that.statesize &&
                 duration == that.duration &&
                 aligmentBuffered == that.aligmentBuffered &&
-                processedData == processedData &&       
+                processedData == that.processedData &&        // This was changed.
                 persistedData = that.persistedData &&
                 numSubtasks = that.numSubtask &&
                 numAckSubtasks == that.numAckSubtasks &&
